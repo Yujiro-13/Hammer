d82383f3a3403daf17f4dd2dba7dfe16f129e7de
Yujiro-13 <yujiro.sss.2022@outlook.jp>
2024-01-23 21:47:56 +0900

    [Add] New test class

--- main/include/MIcromouse/UI/test.hpp
+++ main/include/MIcromouse/UI/test.hpp
@@ -15,7 +15,7 @@ class Test : public UI
         void main_task() override;
         void ref_by_motion(Adachi &_adachi) override;
     private:
-        t_sens_data *sens;   // 後でexternの方を消し、こっちに書き換える
+        t_sens_data *sens;    
         t_mouse_motion_val *val;
         t_control *control;
         t_map *map;
@@ -33,7 +33,7 @@ class Test2 : public UI
         void main_task() override;
         void ref_by_motion(Adachi &_adachi) override;
     private:
-        t_sens_data *sens;   // 後でexternの方を消し、こっちに書き換える
+        t_sens_data *sens;    
         t_mouse_motion_val *val;
         t_control *control;
         t_map *map;
@@ -51,7 +51,7 @@ class Test3 : public UI
         void main_task() override;
         void ref_by_motion(Adachi &_adachi) override;
     private:
-        t_sens_data *sens;   // 後でexternの方を消し、こっちに書き換える
+        t_sens_data *sens;    
         t_mouse_motion_val *val;
         t_control *control;
         t_map *map;
@@ -69,7 +69,61 @@ class Test4 : public UI
         void main_task() override;
         void ref_by_motion(Adachi &_adachi) override;
     private:
-        t_sens_data *sens;   // 後でexternの方を消し、こっちに書き換える
+        t_sens_data *sens;    
+        t_mouse_motion_val *val;
+        t_control *control;
+        t_map *map;
+        Adachi motion;
+};
+
+class Test5 : public UI
+{
+    public:
+        void ptr_by_sensor(t_sens_data *_sens) override;
+        void ptr_by_motion(t_mouse_motion_val *_val) override;
+        void ptr_by_control(t_control *_control) override;
+        void ptr_by_map(t_map *_map) override;
+        void set_module(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) override;
+        void main_task() override;
+        void ref_by_motion(Adachi &_adachi) override;
+    private:
+        t_sens_data *sens;    
+        t_mouse_motion_val *val;
+        t_control *control;
+        t_map *map;
+        Adachi motion;
+};
+
+class Test6 : public UI
+{
+    public:
+        void ptr_by_sensor(t_sens_data *_sens) override;
+        void ptr_by_motion(t_mouse_motion_val *_val) override;
+        void ptr_by_control(t_control *_control) override;
+        void ptr_by_map(t_map *_map) override;
+        void set_module(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) override;
+        void main_task() override;
+        void ref_by_motion(Adachi &_adachi) override;
+    private:
+        t_sens_data *sens;    
+        t_mouse_motion_val *val;
+        t_control *control;
+        t_map *map;
+        Adachi motion;
+};
+
+class Test7 : public UI
+{
+    public:
+        void ptr_by_sensor(t_sens_data *_sens) override;
+        void ptr_by_motion(t_mouse_motion_val *_val) override;
+        void ptr_by_control(t_control *_control) override;
+        void ptr_by_map(t_map *_map) override;
+        void set_module(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) override;
+        void main_task() override;
+        void ref_by_motion(Adachi &_adachi) override;
+    private:
+        t_sens_data *sens;    
         t_mouse_motion_val *val;
         t_control *control;
         t_map *map;
--- main/test.cpp
+++ main/test.cpp
@@ -75,3 +75,61 @@ void Test4::main_task()
     motion.turn_right();
     std::cout << "Test4" << std::endl;
 }
+
+void Test5::ptr_by_sensor(t_sens_data *_sens) { sens = _sens; }
+
+void Test5::ptr_by_motion(t_mouse_motion_val *_val) { val = _val; }
+
+void Test5::ptr_by_control(t_control *_control) { control = _control; }
+
+void Test5::ptr_by_map(t_map *_map) { map = _map; }
+
+void Test5::set_module(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
+
+void Test5::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
+
+void Test5::main_task()
+{   
+    control->log_flag = TRUE;
+    motion.turn_half();
+    std::cout << "Test" << std::endl;
+}
+
+void Test6::ptr_by_sensor(t_sens_data *_sens) { sens = _sens; }
+
+void Test6::ptr_by_motion(t_mouse_motion_val *_val) { val = _val; }
+
+void Test6::ptr_by_control(t_control *_control) { control = _control; }
+
+void Test6::ptr_by_map(t_map *_map) { map = _map; }
+
+void Test6::set_module(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
+
+void Test6::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
+
+void Test6::main_task()
+{
+    control->log_flag = TRUE;
+    motion.stop();
+    std::cout << "Test2" << std::endl;
+}
+
+void Test7::ptr_by_sensor(t_sens_data *_sens) { sens = _sens; }
+
+void Test7::ptr_by_motion(t_mouse_motion_val *_val) { val = _val; }
+
+void Test7::ptr_by_control(t_control *_control) { control = _control; }
+
+void Test7::ptr_by_map(t_map *_map) { map = _map; }
+
+void Test7::set_module(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
+
+void Test7::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
+
+void Test7::main_task()
+{
+    control->log_flag = TRUE;
+    motion.run_half();
+    std::cout << "Test3" << std::endl;
+}
+
