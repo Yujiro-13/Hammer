4cacf06128c85d32c0beba292abbc7907c858f56
Yujiro-13 <yujiro.sss.2022@outlook.jp>
2024-02-18 04:42:56 +0900

    [Fix] Fast mode 開始時rebootされる問題を修正

--- main/fast.cpp
+++ main/fast.cpp
@@ -8,12 +8,25 @@ void Fast::ptr_by_control(t_control *_control) { control = _control; }
 
 void Fast::ptr_by_map(t_map *_map) { map = _map; }
 
-void Fast::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot){}
+void Fast::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
 
-void Fast::ref_by_motion(Adachi &_adachi) {}
+void Fast::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
 
 void Fast::main_task()
 {
+    val->current.rad = 0.0;
+    map->pos.x = 0;
+    map->pos.y = 0;
+    map->pos.dir = NORTH;
+    // map->flag = SEARCH;
+    control->log_flag = TRUE;
+    motion.InitMaze();
+    *map = map_read();
+    map->search_count_flag = TRUE;
+    map->search_time = 0;
+    motion.fast_run(map->GOAL_X, map->GOAL_Y);
+    control->log_flag = FALSE;
+
     std::cout << "Fast" << std::endl;
 }
 
@@ -25,9 +38,9 @@ void Fast2::ptr_by_control(t_control *_control) { control = _control; }
 
 void Fast2::ptr_by_map(t_map *_map) { map = _map; }
 
-void Fast2::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot){}
+void Fast2::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
 
-void Fast2::ref_by_motion(Adachi &_adachi) {}
+void Fast2::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
 
 void Fast2::main_task()
 {
@@ -42,9 +55,9 @@ void Fast3::ptr_by_control(t_control *_control) { control = _control; }
 
 void Fast3::ptr_by_map(t_map *_map) { map = _map; }
 
-void Fast3::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot){}
+void Fast3::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
 
-void Fast3::ref_by_motion(Adachi &_adachi) {}
+void Fast3::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
 
 void Fast3::main_task()
 {
@@ -59,9 +72,9 @@ void Fast4::ptr_by_control(t_control *_control) { control = _control; }
 
 void Fast4::ptr_by_map(t_map *_map) { map = _map; }
 
-void Fast4::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot){}
+void Fast4::set_device(ADC &_adc, AS5047P &_encR, AS5047P &_encL, BUZZER &_buz, MPU6500 &_imu, PCA9632 &_led, Motor &_mot) {}
 
-void Fast4::ref_by_motion(Adachi &_adachi) {}
+void Fast4::ref_by_motion(Adachi &_adachi) { motion = _adachi; }
 
 void Fast4::main_task()
 {
